ed41fb4559a353cb78a937587845a35ed79d35e9
CGevorg <CGevorgHarutyunyan@gmail.com>
2020-08-11 06:33:58 -0700

    Adding a comment to controllerAdvice class.

--- src/main/java/com/cgev/rest/RestStandardDemo/model/EmployeeEntity.java
+++ src/main/java/com/cgev/rest/RestStandardDemo/model/EmployeeEntity.java
@@ -1,14 +1,10 @@
 package com.cgev.rest.RestStandardDemo.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
